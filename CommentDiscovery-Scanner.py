783ed0d778aa1dbc60894365c09500659514e6db
kompotkot <7047457+kompotkot@users.noreply.github.com>
2019-10-15 15:29:32 +0300

    Update CommentDiscovery-Scanner.py

--- CommentDiscovery-Scanner.py
+++ CommentDiscovery-Scanner.py
@@ -9,7 +9,6 @@ import time
 urlList = str(sys.argv[1])
 generatedFile = '3-CommentDiscovery.txt'
 
-
 def getResponse(someUrl):
 	try:
 		r = requests.get(someUrl, timeout=5)
@@ -23,7 +22,6 @@ def getResponse(someUrl):
 		return str(r.status_code)
 		#return err
 
-
 def fileWriter(urlList, toFile=generatedFile):
 	tof = open(toFile, 'w')
 
@@ -34,31 +32,23 @@ def fileWriter(urlList, toFile=generatedFile):
 
 	tof.close()
 
-
 def commentFilter(someLst):
 	comment = ''
 
 	for i in range(len(someLst)):
 		cnt = 0
 		if someLst[i] == '<' and someLst[i+1] == '!' and someLst[i+2] == '-' and someLst[i+3] == '-':	# Check when comment starts with <!--
-			cnt = i + 3		# Change to 4 if you don't want to see '-' before each comment
-
-			while someLst[cnt] != '-' or someLst[cnt+1] != '>':	# Check if comment end with -->
+			cnt = i + 3
+			while someLst[cnt] != '-' or someLst[cnt+1] != '>':
 				comment += someLst[cnt]
 				cnt += 1
-			
 			comment += '\n\n'
-
 	return comment
-
 
 def main():
 	startTime = time.time()
-
 	fileWriter(urlList)
-
-	print(time.time() - startTime)		# Screen-speed timer
-
+	print(time.time() - startTime)	
 
 if __name__ == '__main__':
 	main()
