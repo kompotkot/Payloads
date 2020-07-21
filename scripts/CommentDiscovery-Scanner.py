66d3873662567773360b50cb364a3d8cfb6959f9
kompotkot <zenitsws@gmail.com>
2020-07-21 11:59:37 +0300

    Rewrite in asyncio/aiohttp + 20% speed

--- scripts/CommentDiscovery-Scanner.py
+++ scripts/CommentDiscovery-Scanner.py
@@ -1,8 +1,9 @@
+import asyncio
+import aiohttp
+
 import time
 import argparse
-import requests
 from collections import ChainMap
-
 
 defaults = {'list': 'urllist.txt', 'output': 'urlcomments.txt'}
 
@@ -14,46 +15,59 @@ args = parser.parse_args()
 new_dict = {key: value for key, value in vars(args).items() if value}
 settings = ChainMap(new_dict, defaults)
 
-def getResponse(someUrl):
-	try:
-		r = requests.get(someUrl, timeout=5)
-		r.raise_for_status()
-		#return str(r.status_code)
-		chars = []
-		for i in r.text:
-			chars.append(i)		# Get all body as list of chars
-		return chars
-	except requests.exceptions.RequestException as err:
-		return str(r.status_code)
-		#return err
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        # print('Status:', response.status)
+        # print('Content-type:', response.headers['content-type'])
+        html = await response.text()
+        someComments = commentFilter(html.strip())  # strip() to delete \n
+        return someComments
+
+
+async def request(url):
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, url)
+        return html
+
 
 def fileWriter(urlList, toFile):
-	tof = open(toFile, 'w')
+    tof = open(toFile, 'w')
 
-	with open(urlList) as fp:
-		for line in fp:
-			someComments = commentFilter(getResponse(line.strip()))		# strip() to delete \n
-			tof.write(line + '\n' + someComments + '\n')
+    with open(urlList) as fp:
+        for line in fp:
+            print('Url:', line.strip())
 
-	tof.close()
+            loop = asyncio.get_event_loop()
+            html = loop.run_until_complete(request(line.strip()))
+
+            tof.write(line + '\n' + html + '\n')
+
+    tof.close()
+
 
 def commentFilter(someLst):
-	comment = ''
+    comment = ''
 
-	for i in range(len(someLst)):
-		cnt = 0
-		if someLst[i] == '<' and someLst[i+1] == '!' and someLst[i+2] == '-' and someLst[i+3] == '-':	# Check when comment starts with <!--
-			cnt = i + 3
-			while someLst[cnt] != '-' or someLst[cnt+1] != '>':
-				comment += someLst[cnt]
-				cnt += 1
-			comment += '\n\n'
-	return comment
+    for i in range(len(someLst)):
+        cnt = 0
+        if someLst[i] == '<' and someLst[i + 1] == '!' and someLst[i + 2] == '-' and someLst[
+            i + 3] == '-':  # Check when comment starts with <!--
+            cnt = i + 3
+            while someLst[cnt] != '-' or someLst[cnt + 1] != '>':
+                comment += someLst[cnt]
+                cnt += 1
+            comment += '\n\n'
+    return comment
+
 
 def main():
-	startTime = time.time()
-	fileWriter(settings['list'], settings['output'])
-	print(f'Required time: {time.time() - startTime}')
+    startTime = time.time()
+
+    fileWriter(settings['list'], settings['output'])
+
+    print(f'Required time: {time.time() - startTime}')
+
 
 if __name__ == '__main__':
-	main()
+    main()
