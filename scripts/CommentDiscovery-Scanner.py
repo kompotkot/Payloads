c32a6d2ebacefe00d2df4b8da4cfc3ef7b2cc21a
kompotkot <zenitsws@gmail.com>
2020-07-21 12:42:55 +0300

    Changed to asyncio/aiohttp + 80% speed

--- scripts/CommentDiscovery-Scanner.py
+++ scripts/CommentDiscovery-Scanner.py
@@ -1,50 +1,54 @@
 import asyncio
 import aiohttp
 
+import json
 import time
 import argparse
 from collections import ChainMap
 
-defaults = {'list': 'urllist.txt', 'output': 'urlcomments.txt'}
+defaults = {'list': 'urllist.txt', 'output': 'urlcomments.json'}
 
 parser = argparse.ArgumentParser(description='Comment discovery scanner in code.')
 parser.add_argument('-l', '--list', help='Source of urls list file for scan (default: urllist.txt)')
-parser.add_argument('-o', '--output', help='The file you want to save found comments (default: urlcomments.txt)')
+parser.add_argument('-o', '--output', help='The file you want to save found comments (default: urlcomments.json)')
 
 args = parser.parse_args()
 new_dict = {key: value for key, value in vars(args).items() if value}
 settings = ChainMap(new_dict, defaults)
 
+# Dictionary with urls and comments we will write to file
+result_dct = {}
+
+
+def write_to_file(toFile, data):
+    with open(toFile, 'w') as fp:
+        json.dump(data, fp)
+
 
 async def fetch(session, url):
     async with session.get(url) as response:
+        print('Url:', url)
         # print('Status:', response.status)
         # print('Content-type:', response.headers['content-type'])
         html = await response.text()
-        someComments = commentFilter(html.strip())  # strip() to delete \n
-
-        return someComments
+        html_comments = commentFilter(html)  # Add strip() to delete \n
+        return html_comments
 
 
 async def request(url):
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, url)
-        return html
+        html_comments = await fetch(session, url)
+        result_dct[url] = html_comments
+        # return html_comments
 
 
 def fileWriter(urlList, toFile):
-    tof = open(toFile, 'w')
-
     with open(urlList) as fp:
-        for line in fp:
-            print('Url:', line.strip())
+        loop = asyncio.get_event_loop()
+        tasks = [asyncio.ensure_future(request(line.strip())) for line in fp]       # Task - url(line) in file
+        loop.run_until_complete(asyncio.gather(*tasks))
 
-            loop = asyncio.get_event_loop()
-            html = loop.run_until_complete(request(line.strip()))
-
-            tof.write(line + '\n' + html + '\n')
-
-    tof.close()
+    write_to_file(toFile, result_dct)
 
 
 def commentFilter(someLst):
